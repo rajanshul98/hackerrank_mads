a12fb6c0b2e2310509c203d8f9790ae483c6c589
rajanshul98 <59284601+rajanshul98@users.noreply.github.com>
2020-07-22 11:58:54 +0530

    Update SortBinaryArray.java

--- Practice/DataStructure/Arrays/SortBinaryArray.java
+++ Practice/DataStructure/Arrays/SortBinaryArray.java
@@ -20,6 +20,31 @@ class SortBinaryArray{
             
         return arr;
     }  
+	
+    static int[] SortBinaryArray2(int arr[], int n) 
+    { 
+        //this function uses two pointer concept
+        int i = 0, j = n-1;
+        while(i <= j){
+            if(arr[i] == 1 && arr[j] == 1){
+                j--;
+            }
+            else if(arr[i] == 0 && arr[j] == 0){
+                i++;
+            }
+            else if(arr[i] == 0 && arr[j] == 1){
+                i++;
+                j--;
+            }
+            else if(arr[i] == 1 && arr[j] == 0){
+                arr[i] = 0;
+                arr[j] = 1;
+                i++;
+                j--;
+            }
+        }
+        return arr;
+    }
 
     public static void main(String args[]) throws IOException { 
         Scanner sc = new Scanner(System.in);
