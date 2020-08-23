c936242c7c006c5f14a20bee70577913aeca42ce
rajanshul98 <59284601+rajanshul98@users.noreply.github.com>
2020-08-23 20:19:35 +0530

    Create ValidString.java
    You get the question details here :- https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
    
    Appraoch :- 
    step 1 :- count the frequency of each character and store it in array
    step2 :- sort the array of frequency table
    step3 :- store store the min = arr[i], where i = 1st index where value > 0 and max = arr[25]
    step4 :- if max==min return "YES" else (check whether differnce b/w max and min ==1 and max > arr[24]) or (min == 1 && arr[i+1] == max) then return "YES"
    step5 :- return "NO"

---
+++ Practice/Algorithm/String/ValidString.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*; 
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ValidString {
+
+static String isValid(String s) {
+    if(s.isEmpty()) return "NO";
+    if(s.length() <= 3) return "YES";
+
+    int[] letters = new int[26];
+    for(int i = 0; i < s.length(); i++){
+        letters[s.charAt(i) - 'a']++;
+    }
+    Arrays.sort(letters);
+    int i=0;
+    while(letters[i]==0){
+        i++;
+    }
+    //System.out.println(Arrays.toString(letters));
+    int min = letters[i];   //the smallest frequency of some letter
+    int max = letters[25]; // the largest frequency of some letter
+    String ret = "NO";
+    if(min == max) ret = "YES";
+    else{
+        // remove one letter at higher frequency or the lower frequency 
+        if(((max - min == 1) && (max > letters[24])) ||
+            (min == 1) && (letters[i+1] == max))
+            ret = "YES";
+    }
+    return ret;
+}
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = isValid(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
